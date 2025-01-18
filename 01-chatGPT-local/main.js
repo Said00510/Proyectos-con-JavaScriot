1a2ec240a4e33d5a45267a170ce0035ba0039319
Said00510 <dylanrivas200206@gmail.com>
2025-01-17 23:58:04 -0500

    agregar js

---
+++ 01-chatGPT-local/main.js
@@ -0,0 +1,8 @@
+import { CreateWebWorkerMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
+
+const $ = el => document.querySelector(el)
+
+const $form = $.querySelector('form')
+const $input = $.querySelector('input')
+const $button = $.querySelector('button')
+const $
