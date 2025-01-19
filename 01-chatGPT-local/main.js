d46129220e81ad0bd1d814c923ead1ec41376b41
Said00510 <dylanrivas200206@gmail.com>
2025-01-18 23:00:20 -0500

    agregar funcionalidad basica para comprobar que todo funcionara good

--- 01-chatGPT-local/main.js
+++ 01-chatGPT-local/main.js
@@ -2,7 +2,38 @@ import { CreateWebWorkerMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/w
 
 const $ = el => document.querySelector(el)
 
-const $form = $.querySelector('form')
-const $input = $.querySelector('input')
-const $button = $.querySelector('button')
-const $
+const $form = $('form')
+const $input = $('textarea')
+const $template = $('#message-template')
+const $messages = $('ul')
+const $container = $('main')
+const $button = $('button')
+const $info = $('small')
+const $loading = $('.loading')
+
+$form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const messageText = $input.value.trim()
+    if(messageText !== ""){
+        $input.value = ""
+    }
+
+    addMessage(messageText, "user")
+})
+
+function addMessage (text, sender){
+    //Clonar el template de manera profunda
+    const clonTemplate = $template.content.cloneNode(true)
+    const $newMessage = clonTemplate.querySelector('.message')
+    const $whoSendImage = $newMessage.querySelector('span img')
+    const $text = $newMessage.querySelector('p')
+    const classSender = sender === "bot" ? "bot" : "user"
+
+    $whoSendImage.src = sender === "bot" ? './arabia.png' : './user.png';
+    $text.textContent = text
+    //actualizar scroll
+    
+    $container.scrollTop = $container.scrollHeight
+    $newMessage.classList.add(classSender)
+    $messages.appendChild($newMessage)
+}
