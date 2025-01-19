673cb178edd98790ffe42f4402a9d576f8291116
Said00510 <dylanrivas200206@gmail.com>
2025-01-19 00:56:17 -0500

    agregar logica de enviar y recibir mensaje del bot(falta todaiva arreglar un error)

--- 01-chatGPT-local/main.js
+++ 01-chatGPT-local/main.js
@@ -1,4 +1,4 @@
-import { CreateWebWorkerMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
+import { CreateMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
 
 const $ = el => document.querySelector(el)
 
@@ -11,14 +11,47 @@ const $button = $('button')
 const $info = $('small')
 const $loading = $('.loading')
 
-$form.addEventListener('submit', (event) => {
+const MODEL = "WizardMath-7B-V1.1-q4f16_1-MLC"
+
+let messages = []
+
+const engeine = await CreateMLCEngine(
+    MODEL,
+    {
+        initProgressCallback: (info) => {
+            $info.textContent = info.text
+            if(info.progress === 1){
+                $button.removeAttribute('disabled')
+            }
+        }
+    }
+)
+
+$form.addEventListener('submit', async (event) => {
     event.preventDefault()
     const messageText = $input.value.trim()
     if(messageText !== ""){
         $input.value = ""
     }
-
     addMessage(messageText, "user")
+
+    const userMessages = {
+        role : 'user',
+        content: messageText
+    }
+
+    messages.push(userMessages)
+
+    //PEDIR RESPUESTA DEL BOT
+    const reply = await engeine.chat.completions.create({
+        messages
+    })
+    console.log({reply})
+
+    const botAnswer = reply.choices[0].message
+    messages.push(botAnswer)
+    addMessage(botAnswer.content , 'bot')
+
 })
 
 function addMessage (text, sender){
